673b70b0f5ee622f6f2706277dd2b743433fbf39
nicolas-interhaus <izamorano@interhaus.cl>
2023-01-25 09:00:37 -0300

    25/01/2023

--- new_module/models/prueba chica.py
+++ new_module/models/prueba chica.py
@@ -5,5 +5,19 @@ class pruebas():
         __id=identificador
         __nombre=nombre
         __edad=edad
+
+    def get_id(self):
+        return self.get_id
     
+    def get_nombre(self):
+        return self.get_nombre
+    
+    def get_edad(self):
+        return self.get_edad
+
+        if self.get_nombre()=="nicolas":
+            self.get_id()=1
+            self.get_edad()=23
+            print(self.get_id()," | ",self.get_edad())
+        
     
