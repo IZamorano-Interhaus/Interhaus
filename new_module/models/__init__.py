763ff6cb25011d9e97383b0c572a819497fa6d01
nicolas-interhaus <izamorano@interhaus.cl>
2023-01-25 11:03:35 -0300

    correcion menor 3.1.2

--- new_module/models/__init__.py
+++ new_module/models/__init__.py
@@ -3,6 +3,4 @@
 from . import models
 from . import import_data
 from . import listado_request
-from . import account_account_type
-from . import account_financial_report
-from . import account_move_line
+
