07edd8c0f447979f21b52d252987417e679a8252
nicolas-interhaus <izamorano@interhaus.cl>
2023-01-26 10:20:29 -0300

    update 4.2

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-#a
-import ast
+
 from odoo import models, fields, api
 
 
@@ -16,180 +15,17 @@ class new_module(models.Model):
     documento = fields.Char()
     tipo_documento = fields.Integer()
 
-class AccountAccountType(models.Model):
-    _name = "account.account.type"
-    _description = "Account Account Type"
-
-    name = fields.Char('Name', required=True, translate=True)
-    type = fields.Selection(
-        selection=[
-            ("asset_receivable", "Receivable"),
-            ("asset_cash", "Bank and Cash"),
-            ("asset_current", "Current Assets"),
-            ("asset_non_current", "Non-current Assets"),
-            ("asset_prepayments", "Prepayments"),
-            ("asset_fixed", "Fixed Assets"),
-            ("liability_payable", "Payable"),
-            ("liability_credit_card", "Credit Card"),
-            ("liability_current", "Current Liabilities"),
-            ("liability_non_current", "Non-current Liabilities"),
-            ("equity", "Equity"),
-            ("equity_unaffected", "Current Year Earnings"),
-            ("income", "Income"),
-            ("income_other", "Other Income"),
-            ("expense", "Expenses"),
-            ("expense_depreciation", "Depreciation"),
-            ("expense_direct_cost", "Cost of Revenue"),
-            ("off_balance", "Off-Balance Sheet"),
-        ],
-        string="Type",
-        help="These types are defined according to your country. The type contains more information " \
-             "about the account and its specificities."
-    )
-class AccountFinancialReport(models.Model):  
-    _name = "account.financial.report"
-    _description = "Account Report"
     
+
+    
+
+   
     @api.depends('value')
+
+    
     def _value_pc(self):
         for record in self:
             record.descuento = float(record.value) * 0.10
-    
-    @api.depends('parent_id', 'parent_id.level')
-    def _get_level(self):
-        '''Returns a dictionary with key=the ID of a record and value = the level of this
-           record in the tree structure.'''
-        for report in self:
-            level = 0
-            if report.parent_id:
-                level = report.parent_id.level + 1
-            report.level = level
-
-    def _get_children_by_order(self):
-        '''returns a recordset of all the children computed recursively, and sorted by sequence. Ready for the printing'''
-        res = self
-        children = self.search([('parent_id', 'in', self.ids)], order='sequence ASC')
-        if children:
-            for child in children:
-                res += child._get_children_by_order()
-        return res
-
-    name = fields.Char('Report Name', required=True, translate=True)
-    parent_id = fields.Many2one('account.financial.report', 'Parent')
-    children_ids = fields.One2many('account.financial.report', 'parent_id', 'Account Report')
-    sequence = fields.Integer('Sequence')
-    level = fields.Integer(compute='_get_level', string='Level', store=True, recursive=True)
-    type = fields.Selection([
-        ('sum', 'View'),
-        ('accounts', 'Accounts'),
-        ('account_type', 'Account Type'),
-        ('account_report', 'Report Value'),
-        ], 'Type', default='sum')
-    account_ids = fields.Many2many('account.account', 'account_account_financial_report',
-                                   'report_line_id', 'account_id', 'Accounts')
-    account_report_id = fields.Many2one('account.financial.report', 'Report Value')
-    account_type_ids = fields.Many2many('account.account.type', 'account_account_financial_report_type',
-                                        'report_id', 'account_type_id', 'Account Types')
-    report_domain = fields.Char(string="Report Domain")
-    sign = fields.Selection([('-1', 'Reverse balance sign'), ('1', 'Preserve balance sign')], 'Sign on Reports',
-                            required=True, default='1',
-                            help='For accounts that are typically more debited than credited and that you would'
-                                 ' like to print as negative amounts in your reports, you should reverse the sign'
-                                 ' of the balance; e.g.: Expense account. The same applies for accounts that are '
-                                 'typically more credited than debited and that you would like to print as positive '
-                                 'amounts in your reports; e.g.: Income account.')
-    display_detail = fields.Selection([
-        ('no_detail', 'No detail'),
-        ('detail_flat', 'Display children flat'),
-        ('detail_with_hierarchy', 'Display children with hierarchy')
-        ], 'Display details', default='detail_flat')
-    style_overwrite = fields.Selection([
-        ('0', 'Automatic formatting'),
-        ('1', 'Main Title 1 (bold, underlined)'),
-        ('2', 'Title 2 (bold)'),
-        ('3', 'Title 3 (bold, smaller)'),
-        ('4', 'Normal Text'),
-        ('5', 'Italic Text (smaller)'),
-        ('6', 'Smallest Text'),
-        ], 'Financial Report Style', default='0',
-        help="You can set up here the format you want this record to be displayed. "
-             "If you leave the automatic formatting, it will be computed based on the "
-             "financial reports hierarchy (auto-computed field 'level').")
-    children_ids = fields.One2many('account.financial.report', 'parent_id', string='Children')
-class AccountMoveLine(models.Model):
-    _inherit = "account.move.line"
-
-    @api.model
-    def _query_get(self, domain=None):
-        self.check_access_rights('read')
-
-        context = dict(self._context or {})
-        domain = domain or []
-        if not isinstance(domain, (list, tuple)):
-            domain = ast.literal_eval(domain)
-
-        date_field = 'date'
-        if context.get('aged_balance'):
-            date_field = 'date_maturity'
-        if context.get('date_to'):
-            domain += [(date_field, '<=', context['date_to'])]
-        if context.get('date_from'):
-            if not context.get('strict_range'):
-                domain += ['|', (date_field, '>=', context['date_from']), ('account_id.include_initial_balance', '=', True)]
-            elif context.get('initial_bal'):
-                domain += [(date_field, '<', context['date_from'])]
-            else:
-                domain += [(date_field, '>=', context['date_from'])]
-
-        if context.get('journal_ids'):
-            domain += [('journal_id', 'in', context['journal_ids'])]
-
-        state = context.get('state')
-        if state and state.lower() != 'all':
-            domain += [('parent_state', '=', state)]
-
-        if context.get('company_id'):
-            domain += [('company_id', '=', context['company_id'])]
-        elif context.get('allowed_company_ids'):
-            domain += [('company_id', 'in', self.env.companies.ids)]
-        else:
-            domain += [('company_id', '=', self.env.company.id)]
-
-        if context.get('reconcile_date'):
-            domain += ['|', ('reconciled', '=', False), '|', ('matched_debit_ids.max_date', '>', context['reconcile_date']), ('matched_credit_ids.max_date', '>', context['reconcile_date'])]
-
-        if context.get('account_tag_ids'):
-            domain += [('account_id.tag_ids', 'in', context['account_tag_ids'].ids)]
-
-        if context.get('account_ids'):
-            domain += [('account_id', 'in', context['account_ids'].ids)]
-
-        if context.get('analytic_tag_ids'):
-            domain += [('analytic_tag_ids', 'in', context['analytic_tag_ids'].ids)]
-
-        if context.get('analytic_account_ids'):
-            domain += [('analytic_account_id', 'in', context['analytic_account_ids'].ids)]
-
-        if context.get('partner_ids'):
-            domain += [('partner_id', 'in', context['partner_ids'].ids)]
-
-        if context.get('partner_categories'):
-            domain += [('partner_id.category_id', 'in', context['partner_categories'].ids)]
-
-        where_clause = ""
-        where_clause_params = []
-        tables = ''
-        if domain:
-            domain.append(('display_type', 'not in', ('line_section', 'line_note')))
-            domain.append(('parent_state', '!=', 'cancel'))
-
-            query = self._where_calc(domain)
-
-            # Wrap the query with 'company_id IN (...)' to avoid bypassing company access rights.
-            self._apply_ir_rules(query)
-
-            tables, where_clause, where_clause_params = query.get_sql()
-        return tables, where_clause, where_clause_params
         
   
     
