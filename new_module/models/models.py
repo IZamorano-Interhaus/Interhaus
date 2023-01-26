7808cfc6847092f62f04add2d407839c5ec540dd
nicolas-interhaus <izamorano@interhaus.cl>
2023-01-26 17:54:49 -0300

    update 4.5

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -210,3 +210,154 @@ class PurchaseRequest(models.Model):
         string="Total Estimated Cost",
         store=True,
     )
+    @api.depends("line_ids", "line_ids.estimated_cost")
+    def _compute_estimated_cost(self):
+        for rec in self:
+            rec.estimated_cost = sum(rec.line_ids.mapped("estimated_cost"))
+
+    @api.depends("line_ids")
+    def _compute_purchase_count(self):
+        for rec in self:
+            rec.purchase_count = len(rec.mapped("line_ids.purchase_lines.order_id"))
+
+    def action_view_purchase_order(self):
+        action = self.env["ir.actions.actions"]._for_xml_id("purchase.purchase_rfq")
+        lines = self.mapped("line_ids.purchase_lines.order_id")
+        if len(lines) > 1:
+            action["domain"] = [("id", "in", lines.ids)]
+        elif lines:
+            action["views"] = [
+                (self.env.ref("purchase.purchase_order_form").id, "form")
+            ]
+            action["res_id"] = lines.id
+        return action
+
+    @api.depends("line_ids")
+    def _compute_move_count(self):
+        for rec in self:
+            rec.move_count = len(
+                rec.mapped("line_ids.purchase_request_allocation_ids.stock_move_id")
+            )
+
+    def action_view_stock_picking(self):
+        action = self.env["ir.actions.actions"]._for_xml_id(
+            "stock.action_picking_tree_all"
+        )
+        # remove default filters
+        action["context"] = {}
+        lines = self.mapped(
+            "line_ids.purchase_request_allocation_ids.stock_move_id.picking_id"
+        )
+        if len(lines) > 1:
+            action["domain"] = [("id", "in", lines.ids)]
+        elif lines:
+            action["views"] = [(self.env.ref("stock.view_picking_form").id, "form")]
+            action["res_id"] = lines.id
+        return action
+
+    @api.depends("line_ids")
+    def _compute_line_count(self):
+        for rec in self:
+            rec.line_count = len(rec.mapped("line_ids"))
+
+    def action_view_purchase_request_line(self):
+        action = (
+            self.env.ref("purchase_request.purchase_request_line_form_action")
+            .sudo()
+            .read()[0]
+        )
+        lines = self.mapped("line_ids")
+        if len(lines) > 1:
+            action["domain"] = [("id", "in", lines.ids)]
+        elif lines:
+            action["views"] = [
+                (self.env.ref("purchase_request.purchase_request_line_form").id, "form")
+            ]
+            action["res_id"] = lines.ids[0]
+        return action
+
+    @api.depends("state", "line_ids.product_qty", "line_ids.cancelled")
+    def _compute_to_approve_allowed(self):
+        for rec in self:
+            rec.to_approve_allowed = rec.state == "draft" and any(
+                not line.cancelled and line.product_qty for line in rec.line_ids
+            )
+
+    def copy(self, default=None):
+        default = dict(default or {})
+        self.ensure_one()
+        default.update({"state": "draft", "name": self._get_default_name()})
+        return super(PurchaseRequest, self).copy(default)
+
+    @api.model
+    def _get_partner_id(self, request):
+        user_id = request.assigned_to or self.env.user
+        return user_id.partner_id.id
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get("name", _("New")) == _("New"):
+                vals["name"] = self._get_default_name()
+        requests = super(PurchaseRequest, self).create(vals_list)
+        for vals, request in zip(vals_list, requests):
+            if vals.get("assigned_to"):
+                partner_id = self._get_partner_id(request)
+                request.message_subscribe(partner_ids=[partner_id])
+        return requests
+
+    def write(self, vals):
+        res = super(PurchaseRequest, self).write(vals)
+        for request in self:
+            if vals.get("assigned_to"):
+                partner_id = self._get_partner_id(request)
+                request.message_subscribe(partner_ids=[partner_id])
+        return res
+
+    def _can_be_deleted(self):
+        self.ensure_one()
+        return self.state == "draft"
+
+    def unlink(self):
+        for request in self:
+            if not request._can_be_deleted():
+                raise UserError(
+                    _("You cannot delete a purchase request which is not draft.")
+                )
+        return super(PurchaseRequest, self).unlink()
+
+    def button_draft(self):
+        self.mapped("line_ids").do_uncancel()
+        return self.write({"state": "draft"})
+
+    def button_to_approve(self):
+        self.to_approve_allowed_check()
+        return self.write({"state": "to_approve"})
+
+    def button_approved(self):
+        return self.write({"state": "approved"})
+
+    def button_rejected(self):
+        self.mapped("line_ids").do_cancel()
+        return self.write({"state": "rejected"})
+
+    def button_done(self):
+        return self.write({"state": "done"})
+
+    def check_auto_reject(self):
+        """When all lines are cancelled the purchase request should be
+        auto-rejected."""
+        for pr in self:
+            if not pr.line_ids.filtered(lambda l: l.cancelled is False):
+                pr.write({"state": "rejected"})
+
+    def to_approve_allowed_check(self):
+        for rec in self:
+            if not rec.to_approve_allowed:
+                raise UserError(
+                    _(
+                        "You can't request an approval for a purchase request "
+                        "which is empty. (%s)"
+                    )
+                    % rec.name
+                )
