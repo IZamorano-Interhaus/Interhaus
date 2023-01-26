4f67034a76e04d913bb8e0caafa41a79e3f79d8d
nicolas-interhaus <izamorano@interhaus.cl>
2023-01-26 15:03:28 -0300

    correcion menor 4.3.2

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -10,11 +10,18 @@ class new_module(models.Model):
     value = fields.Integer()
     descuento = fields.Float(compute="_value_pc", store=True)
     
+    analytic_account_id = fields.Many2one('account.analytic.account', 'Analytic Account')
+    crossovered_budget_line = fields.One2many('crossovered.budget.lines', 'analytic_account_id', 'Budget Lines')
+    crossovered_budget_line = fields.One2many('crossovered.budget.lines', 'crossovered_budget_id', 'Budget Lines',
+        states={'done': [('readonly', True)]}, copy=True)
+
+
+
     rut = fields.Char()
     folio = fields.Char()
-    documento = fields.One2many('documento','rut','documento')
+    documento = fields.One2many('new_module','value','new_module.new_module')
     """ crossovered_budget_line = fields.One2many('crossovered.budget.lines', 'analytic_account_id', 'Budget Lines')  """
-    tipo_documento = fields.One2many('tipo_documento','rut','documento')
+    tipo_documento = fields.One2many('new_module','value','new_module.new_module')
    
     @api.depends('value')
 
