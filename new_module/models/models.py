d7452212fe2274f8c736099084a3c01b196d168b
nicolas-interhaus <izamorano@interhaus.cl>
2023-01-26 16:26:25 -0300

    correcion menor 4.3.4

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class new_module(models.Model):
@@ -19,16 +20,47 @@ class new_module(models.Model):
 
     rut = fields.Char()
     folio = fields.Char()
-    
+    """ documento = fields.One2many('new_module',inverse_name='new_module.new_module')
+    crossovered_budget_line = fields.One2many('crossovered.budget.lines', 'analytic_account_id', 'Budget Lines')  
+    tipo_documento = fields.One2many('new_module',inverse_name='new_module.new_module') """
    
+    documento=fields.Char()
+    tipo_cosumento=fields.Integer()
     @api.depends('value')
 
     
     def _value_pc(self):
         for record in self:
             record.descuento = float(record.value) * 0.10
+
+
+""" class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    edi_document_ids = fields.One2many(
+        comodel_name='account.edi.document',
+        inverse_name='move_id')
+    edi_state = fields.Selection(
+        selection=[('to_send', 'To Send'), ('sent', 'Sent'), ('to_cancel', 'To Cancel'), ('cancelled', 'Cancelled')],
+        string="Electronic invoicing",
+        store=True,
+        compute='_compute_edi_state',
+        help='The aggregated state of all the EDIs with web-service of this move')
+    edi_error_count = fields.Integer(
+        compute='_compute_edi_error_count',
+        help='How many EDIs are in error for this move ?')
+    edi_blocking_level = fields.Selection(
+        selection=[('info', 'Info'), ('warning', 'Warning'), ('error', 'Error')],
+        compute='_compute_edi_error_message')
+    edi_error_message = fields.Html(
+        compute='_compute_edi_error_message')
+    # Technical field to display the documents that will be processed by the CRON
+    edi_web_services_to_process = fields.Text(
+        compute='_compute_edi_web_services_to_process')
+    edi_show_cancel_button = fields.Boolean(
+        compute='_compute_edi_show_cancel_button')
+    edi_show_abandon_cancel_button = fields.Boolean(
+        compute='_compute_edi_show_abandon_cancel_button') """
         
-    documento = fields.One2many('new_module',inverse_name='new_module.new_module')
-    """ crossovered_budget_line = fields.One2many('crossovered.budget.lines', 'analytic_account_id', 'Budget Lines')  """
-    tipo_documento = fields.One2many('new_module',inverse_name='new_module.new_module')
+    
     
