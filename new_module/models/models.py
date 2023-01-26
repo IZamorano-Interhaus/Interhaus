c5db85d4b9f15ad6c71df322c938788043387afd
nicolas-interhaus <izamorano@interhaus.cl>
2023-01-26 17:46:02 -0300

    update 4.5

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 from odoo.exceptions import UserError
-
+_STATES = [
+    ("draft", "Draft"),
+    ("to_approve", "To be approved"),
+    ("approved", "Approved"),
+    ("rejected", "Rejected"),
+    ("done", "Done"),
+]
 
 class new_module(models.Model):
     _name = 'new_module.new_module'
@@ -10,22 +16,25 @@ class new_module(models.Model):
     name = fields.Char()
     value = fields.Integer()
     descuento = fields.Float(compute="_value_pc", store=True)
-    
-    """ analytic_account_id = fields.Many2one('account.analytic.account', 'Analytic Account')
-    crossovered_budget_line = fields.One2many('crossovered.budget.lines', 'analytic_account_id', 'Budget Lines')
-    crossovered_budget_line = fields.One2many('crossovered.budget.lines', 'crossovered_budget_id', 'Budget Lines',
-        states={'done': [('readonly', True)]}, copy=True) """
-
-
-
     rut = fields.Char()
     folio = fields.Char()
-    """ documento = fields.One2many('new_module',inverse_name='new_module.new_module')
-    crossovered_budget_line = fields.One2many('crossovered.budget.lines', 'analytic_account_id', 'Budget Lines')  
-    tipo_documento = fields.One2many('new_module',inverse_name='new_module.new_module') """
-   
     documento=fields.Char()
     tipo_documento=fields.Integer()
+    date_start = fields.Date(
+        string="Fecha Inicio",
+        help="Date when the user initiated the request.",
+        default=fields.Date.context_today,
+        tracking=True,
+    )
+
+    """ documento = fields.One2many('new_module',inverse_name='new_module.new_module')
+    tipo_documento = fields.One2many('new_module',inverse_name='new_module.new_module') 
+    crossovered_budget_line = fields.One2many('crossovered.budget.lines', 'analytic_account_id', 'Budget Lines')
+    analytic_account_id = fields.Many2one('account.analytic.account', 'Analytic Account')
+    crossovered_budget_line = fields.One2many('crossovered.budget.lines', 'analytic_account_id', 'Budget Lines')
+    crossovered_budget_line = fields.One2many('crossovered.budget.lines', 'crossovered_budget_id', 'Budget Lines',
+    states={'done': [('readonly', True)]}, copy=True) """
+    
     @api.depends('value')
 
     
@@ -63,4 +72,141 @@ class new_module(models.Model):
         compute='_compute_edi_show_abandon_cancel_button') """
         
     
-    
+class PurchaseRequest(models.Model):
+
+    _name = "purchase.request"
+    _description = "Purchase Request"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
+    _order = "id desc"
+
+    @api.model
+    def _company_get(self):
+        return self.env["res.company"].browse(self.env.company.id)
+
+    @api.model
+    def _get_default_requested_by(self):
+        return self.env["res.users"].browse(self.env.uid)
+
+    @api.model
+    def _get_default_name(self):
+        return self.env["ir.sequence"].next_by_code("purchase.request")
+
+    @api.model
+    def _default_picking_type(self):
+        type_obj = self.env["stock.picking.type"]
+        company_id = self.env.context.get("company_id") or self.env.company.id
+        types = type_obj.search(
+            [("code", "=", "incoming"), ("warehouse_id.company_id", "=", company_id)]
+        )
+        if not types:
+            types = type_obj.search(
+                [("code", "=", "incoming"), ("warehouse_id", "=", False)]
+            )
+        return types[:1]
+
+    @api.depends("state")
+    def _compute_is_editable(self):
+        for rec in self:
+            if rec.state in ("to_approve", "approved", "rejected", "done"):
+                rec.is_editable = False
+            else:
+                rec.is_editable = True
+
+    name = fields.Char(
+        string="Request Reference",
+        required=True,
+        default=lambda self: _("New"),
+        tracking=True,
+    )
+    is_name_editable = fields.Boolean(
+        default=lambda self: self.env.user.has_group("base.group_no_one"),
+    )
+    origin = fields.Char(string="Source Document")
+    date_start = fields.Date(
+        string="Creation date",
+        help="Date when the user initiated the request.",
+        default=fields.Date.context_today,
+        tracking=True,
+    )
+    requested_by = fields.Many2one(
+        comodel_name="res.users",
+        required=True,
+        copy=False,
+        tracking=True,
+        default=_get_default_requested_by,
+        index=True,
+    )
+    assigned_to = fields.Many2one(
+        comodel_name="res.users",
+        string="Approver",
+        tracking=True,
+        domain=lambda self: [
+            (
+                "groups_id",
+                "in",
+                self.env.ref("purchase_request.group_purchase_request_manager").id,
+            )
+        ],
+        index=True,
+    )
+    description = fields.Text()
+    company_id = fields.Many2one(
+        comodel_name="res.company",
+        required=False,
+        default=_company_get,
+        tracking=True,
+    )
+    line_ids = fields.One2many(
+        comodel_name="purchase.request.line",
+        inverse_name="request_id",
+        string="Products to Purchase",
+        readonly=False,
+        copy=True,
+        tracking=True,
+    )
+    product_id = fields.Many2one(
+        comodel_name="product.product",
+        related="line_ids.product_id",
+        string="Product",
+        readonly=True,
+    )
+    state = fields.Selection(
+        selection=_STATES,
+        string="Status",
+        index=True,
+        tracking=True,
+        required=True,
+        copy=False,
+        default="draft",
+    )
+    is_editable = fields.Boolean(compute="_compute_is_editable", readonly=True)
+    to_approve_allowed = fields.Boolean(compute="_compute_to_approve_allowed")
+    picking_type_id = fields.Many2one(
+        comodel_name="stock.picking.type",
+        string="Picking Type",
+        required=True,
+        default=_default_picking_type,
+    )
+    group_id = fields.Many2one(
+        comodel_name="procurement.group",
+        string="Procurement Group",
+        copy=False,
+        index=True,
+    )
+    line_count = fields.Integer(
+        string="Purchase Request Line count",
+        compute="_compute_line_count",
+        readonly=True,
+    )
+    move_count = fields.Integer(
+        string="Stock Move count", compute="_compute_move_count", readonly=True
+    )
+    purchase_count = fields.Integer(
+        string="Purchases count", compute="_compute_purchase_count", readonly=True
+    )
+    currency_id = fields.Many2one(related="company_id.currency_id", readonly=True)
+    estimated_cost = fields.Monetary(
+        compute="_compute_estimated_cost",
+        string="Total Estimated Cost",
+        store=True,
+    )
