83e3a764128714ad01c40a4235e51e2bead4e3cd
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-01-30 17:02:44 -0300

    cambio 2.6, borrador listo, verificar con detenimiento las vistas y la distribucion de las funciones del modelo

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -10,24 +10,19 @@ _STATES = [
     ("rejected", "Rejected"),
     ("done", "Done"),
 ]
-
 class new_module(models.Model):
     _name = 'new_module.new_module'
-    _description = 'new_module.new_module'
-    
-    
-    
-    name = fields.Char(
+    _description = 'new_module.new_module' 
+    nombre = fields.Char(
         string="Referencia comprador",
         required=True,
         default=lambda self: _("Ejemplo: Nicolas"),
     )
-    rut = fields.Char(
+    rut_tributario = fields.Char(
         string="Rut",
         required=True,
         default=lambda self: _("Sin puntos y con guión"),
-    )
-    
+    )   
     documento=fields.Many2one(
         comodel_name="product.product",
         string="documento",
@@ -51,19 +46,21 @@ class new_module(models.Model):
         help="Date when the user initiated the request.",
         default=fields.Date.context_today,
     )
-    banco_involucrado = fields.Many2one(
-        comodel_name="procurement.group",
-        string="banco",
-        copy=False,
-        index=True,
+    referencia_pago = fields.Char(
+        string="Referencia de pago",
+        help="La referencia de pago para establecer en apuntes de diario.",
     )
     date_factura = fields.Date(
         string="fecha de factura",
         default=fields.Date.context_today
     )
-    date_pago = fields.Date(
-        string="fecha de pago",
+    fecha_vencimiento = fields.Date(
+        string="fecha de vencimiento",
         default=fields.Date.context_today
+    )
+    documento_id = fields.Char(
+        string = "Número de documento",
+        required=True,
     )
     """ is_editable = fields.Boolean(compute="_compute_is_editable", readonly=True) """
 class AccountJournalGroup(models.Model):
