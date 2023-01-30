3ac8620cea6c9e318d9f598d905a9cb0993ff770
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-01-30 08:18:44 -0300

    punto de partida 8:18 30/01/2023

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -14,20 +14,41 @@ _STATES = [
 class new_module(models.Model):
     _name = 'new_module.new_module'
     _description = 'new_module.new_module'
-    name = fields.Char()
+    name = fields.Char(
+        string="Referencia comprador",
+        required=True,
+        default=lambda self: _("Ejemplo: Nicolas"),
+        tracking=True,
+    )
     value = fields.Integer()
     descuento = fields.Float(compute="_value_pc", store=True)
-    rut = fields.Char()
+    rut = fields.Char(
+        string="Rut",
+        required=True,
+        default=lambda self: _("Sin puntos y con guión"),
+        tracking=True,
+    )
     
-    documento=fields.Char()
-    folio = fields.Char()
+    documento=fields.Many2one(
+        comodel_name="product.product",
+        string="documento",
+        copy=False,
+        index=True,
+    )
+
+    folio = fields.Many2one(
+        comodel_name="procurement.group",
+        string="Folio",
+        copy=False,
+        index=True,
+    )
+
     date_start = fields.Date(
         string="Fecha Inicio",
         help="Date when the user initiated the request.",
         default=fields.Date.context_today,
         tracking=True,
     )
-    tipo_documento=fields.Char()
     
     
     """ is_editable = fields.Boolean(compute="_compute_is_editable", readonly=True) """
