ef2e8210c930fdf335622c4a6d72465c5ae51e8d
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-01-30 16:30:36 -0300

    cambio 2.5, se agrega algunos datos de contabilidad a new module

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -51,5 +51,32 @@ class new_module(models.Model):
         help="Date when the user initiated the request.",
         default=fields.Date.context_today,
     )
+    banco_involucrado = fields.Many2one(
+        comodel_name="procurement.group",
+        string="banco",
+        copy=False,
+        index=True,
+    )
+    date_factura = fields.Date(
+        string="fecha de factura",
+        default=fields.Date.context_today
+    )
+    date_pago = fields.Date(
+        string="fecha de pago",
+        default=fields.Date.context_today
+    )
     """ is_editable = fields.Boolean(compute="_compute_is_editable", readonly=True) """
-    
+class AccountJournalGroup(models.Model):
+    _name = 'account.journal.group'
+    _description = "Account Journal Group"
+    _check_company_auto = True
+
+    name = fields.Char("Journal Group", required=True, translate=True)
+    company_id = fields.Many2one('res.company', required=True, default=lambda self: self.env.company)
+    excluded_journal_ids = fields.Many2many('account.journal', string="Excluded Journals", domain="[('company_id', '=', company_id)]",
+        check_company=True)
+    sequence = fields.Integer(default=10)
+
+    _sql_constraints = [
+        ('uniq_name', 'unique(company_id, name)', 'A journal group name must be unique per company.'),
+    ]
