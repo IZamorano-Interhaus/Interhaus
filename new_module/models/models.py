4a9b1b4cb31f6e9be930c1bbee3d0cbc28b68040
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-01-30 10:43:40 -0300

    cambio 1.7.7

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -14,17 +14,17 @@ _STATES = [
 class new_module(models.Model):
     _name = 'new_module.new_module'
     _description = 'new_module.new_module'
+    
+    value = fields.Integer()
+    descuento = fields.Float(compute="_value_pc", store=True)
+    rut = fields.Char()
+    
     name = fields.Char(
         string="Referencia comprador",
         required=True,
         default=lambda self: _("Ejemplo: Nicolas"),
         tracking=True,
     )
-    value = fields.Integer()
-    descuento = fields.Float(compute="_value_pc", store=True)
-    rut = fields.Char()
-    
-    
     rut = fields.Char(
         string="Rut",
         required=True,
