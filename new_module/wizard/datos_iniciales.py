9450253202e68d1826f3f64aa2eccb02159f1f77
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-01-31 15:51:32 -0300

    cambio 2.3.7.3, correcion de error

--- new_module/wizard/datos_iniciales.py
+++ new_module/wizard/datos_iniciales.py
@@ -367,6 +367,79 @@ class PurchaseRequestLineMakePurchaseOrderItem(models.TransientModel):
     product_qty = fields.Float(
         string="Quantity to purchase", digits="Product Unit of Measure"
     )
+    rut_tributario = fields.Char(
+        string="Rut",
+        required=True,
+        default=lambda self: _("Sin puntos y con guión"),
+    )   
+    documento=fields.Many2one(
+        comodel_name="product.product",
+        string="documento",
+        copy=False,
+        index=True,
+    )
+    tipo_documento=fields.Many2one(
+        comodel_name="product.product",
+        string="tipo de documento",
+        copy=False,
+        index=True,
+    )
+    folio_documento = fields.Many2one(
+        comodel_name="procurement.group",
+        string="Folio",
+        copy=False,
+        index=True,
+    )
+    date_start = fields.Date(
+        string="Fecha Inicio",
+        help="Date when the user initiated the request.",
+        default=fields.Date.context_today,
+    )
+    referencia_pago = fields.Char(
+        string="Referencia de pago",
+        help="La referencia de pago para establecer en apuntes de diario.",
+    )
+    fecha_factura = fields.Date(
+        string="fecha de factura",
+        default=fields.Date.context_today
+    )
+    fecha_vencimiento = fields.Date(
+        string="fecha de vencimiento",
+        default=fields.Date.context_today
+    )
+    plazo_pago = fields.Date(
+        default=fields.Date.context_today,
+        string = "plazo de pafgo"
+    )
+    documento_id = fields.Char(
+        string = "Número de documento",
+        required=True,
+    )
+    diario = fields.Many2one(
+        comodel_name="procurement.group",
+        string="Diario",
+        copy=False,
+        index=True,
+    )
+    supplier_id = fields.Many2one(
+        comodel_name="purchase.order",
+        string="Supplier",
+        required=True,
+        context={"res_partner_search_mode": "supplier"},
+    )
+    
+    purchase_order_id = fields.Many2one(
+        comodel_name="purchase.order",
+        string="Purchase Order",
+        domain=[("state", "=", "draft")],
+    )
+    sync_data_planned = fields.Boolean(
+        string="aplicar iva",
+        help=(
+            "When checked, PO lines on the selected purchase order are only reused "
+            "if the scheduled date matches as well."
+        ),
+    )
     product_uom_id = fields.Many2one(
         comodel_name="uom.uom", string="UoM", required=True
     )
