58b54e702f4f48d21b048d080bfa5820c56351c8
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-01-31 10:54:51 -0300

    cambio 2.1.4

--- new_module/wizard/datos_iniciales.py
+++ new_module/wizard/datos_iniciales.py
@@ -9,11 +9,65 @@ from odoo.tools import get_lang
 class initial_data(models.TransientModel):
     _name='new_module.new_module'
     _description='new_module.new_module'
-    
+
     cliente = fields.Char(
         string="Referencia comprador",
         required=True,
         default=lambda self: _("Ejemplo: Nicolas"),
+    )
+    rut_tributario = fields.Char(
+        string="Rut",
+        required=True,
+        default=lambda self: _("Sin puntos y con guión"),
+    )   
+    documento=fields.Many2one(
+        comodel_name="product.product",
+        string="documento",
+        copy=False,
+        index=True,
+    )
+    tipo_documento=fields.Many2one(
+        comodel_name="product.product",
+        string="tipo de documento",
+        copy=False,
+        index=True,
+    )
+    folio_documento = fields.Many2one(
+        comodel_name="procurement.group",
+        string="Folio",
+        copy=False,
+        index=True,
+    )
+    date_start = fields.Date(
+        string="Fecha Inicio",
+        help="Date when the user initiated the request.",
+        default=fields.Date.context_today,
+    )
+    referencia_pago = fields.Char(
+        string="Referencia de pago",
+        help="La referencia de pago para establecer en apuntes de diario.",
+    )
+    fecha_factura = fields.Date(
+        string="fecha de factura",
+        default=fields.Date.context_today
+    )
+    fecha_vencimiento = fields.Date(
+        string="fecha de vencimiento",
+        default=fields.Date.context_today
+    )
+    plazo_pago = fields.Date(
+                default=fields.Date.context_today
+
+    )
+    documento_id = fields.Char(
+        string = "Número de documento",
+        required=True,
+    )
+    diario = fields.Many2one(
+        comodel_name="procurement.group",
+        string="Diario",
+        copy=False,
+        index=True,
     )
     supplier_id = fields.Many2one(
         comodel_name="res.partner",
