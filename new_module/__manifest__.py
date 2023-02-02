06ed6ea29b61d9dea42495fe32120e2aacecbf45
nicolas-interhaus <izamorano@interhaus.cl>
2023-02-02 14:25:07 -0300

    Correccion 2.5.3

--- new_module/__manifest__.py
+++ new_module/__manifest__.py
@@ -25,7 +25,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/models_view',
+        'views/models_view.xml',
         'wizard/datos_iniciales_views.xml'
     ],
     # only loaded in demonstration mode
