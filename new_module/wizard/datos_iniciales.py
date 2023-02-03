f7805a0d5974392f4b4011bdb9b6170fb332854f
nicolas-interhaus <izamorano@interhaus.cl>
2023-02-03 09:17:24 -0300

    03/02/2023, se agrega nuevas variables y metodos a datos_iniciales

--- new_module/wizard/datos_iniciales.py
+++ new_module/wizard/datos_iniciales.py
@@ -1,6 +1,7 @@
 # Copyright 2018-2019 ForgeFlow, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0).
-from datetime import datetime
+from datetime import datetime, date
+from dateutil.relativedelta import relativedelta
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 from odoo.tools import get_lang
@@ -17,12 +18,10 @@ class initial_data(models.TransientModel):
     cliente = fields.Char(
         string="Referencia comprador",
         required=True,
-        default=lambda self: _("Ejemplo: Nicolas"),
     )
     rut_tributario = fields.Char(
         string="Rut",
         required=True,
-        default=lambda self: _("Sin puntos y con guión"),
     )   
     documento=fields.Many2one(
         comodel_name="product.product",
@@ -84,6 +83,200 @@ class initial_data(models.TransientModel):
         string="Purchase Order",
         domain=[("state", "=", "draft")],
     )
+    def _get_next_schedule(self):
+        if self.date:
+            recurr_dates = []
+            today = datetime.today()
+            start_date = datetime.strptime(str(self.date), '%Y-%m-%d')
+            while start_date <= today:
+                recurr_dates.append(str(start_date.date()))
+                if self.recurring_period == 'days':
+                    start_date += relativedelta(days=self.recurring_interval)
+                elif self.recurring_period == 'weeks':
+                    start_date += relativedelta(weeks=self.recurring_interval)
+                elif self.recurring_period == 'months':
+                    start_date += relativedelta(months=self.recurring_interval)
+                else:
+                    start_date += relativedelta(years=self.recurring_interval)
+            self.next_date = start_date.date()
+    name = fields.Char('Name')
+    debit_account = fields.Many2one('account.account', 'Debit Account',
+                                    required=True,
+                                    domain="['|', ('company_id', '=', False), "
+                                           "('company_id', '=', company_id)]")
+    credit_account = fields.Many2one('account.account', 'Credit Account',
+                                     required=True,
+                                     domain="['|', ('company_id', '=', False), "
+                                            "('company_id', '=', company_id)]")
+    journal_id = fields.Many2one('account.journal', 'Journal', required=True)
+    analytic_account_id = fields.Many2one('account.analytic.account',
+                                          'Analytic Account')
+    date = fields.Date('Starting Date', required=True, default=date.today())
+    next_date = fields.Date('Next Schedule', compute=_get_next_schedule,
+                            readonly=True, copy=False)
+    recurring_period = fields.Selection(selection=[('days', 'Days'),
+                                                   ('weeks', 'Weeks'),
+                                                   ('months', 'Months'),
+                                                   ('years', 'Years')],
+                                        store=True, required=True)
+    amount = fields.Float('Amount')
+    description = fields.Text('Description')
+    state = fields.Selection(selection=[('draft', 'Draft'),
+                                        ('running', 'Running')],
+                             default='draft', string='Status')
+    journal_state = fields.Selection(selection=[('draft', 'Unposted'),
+                                                ('posted', 'Posted')],
+                                     required=True, default='draft',
+                                     string='Generate Journal As')
+    recurring_interval = fields.Integer('Recurring Interval', default=1)
+    partner_id = fields.Many2one('res.partner', 'Partner')
+    pay_time = fields.Selection(selection=[('pay_now', 'Pay Directly'),
+                                           ('pay_later', 'Pay Later')],
+                                store=True, required=True)
+    company_id = fields.Many2one('res.company',
+                                 default=lambda l: l.env.company.id)
+    recurring_lines = fields.One2many('account.recurring.entries.line', 'tmpl_id')
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get("name", _("New")) == _("New"):
+                vals["name"] = self._get_default_name()
+        requests = super(initial_data, self).create(vals_list)
+        for vals, request in zip(vals_list, requests):
+            if vals.get("assigned_to"):
+                partner_id = self._get_partner_id(request)
+                request.message_subscribe(partner_ids=[partner_id])
+        return requests
+
+    def write(self, vals):
+        res = super(initial_data, self).write(vals)
+        for request in self:
+            if vals.get("assigned_to"):
+                partner_id = self._get_partner_id(request)
+                request.message_subscribe(partner_ids=[partner_id])
+        return res
+
+    def _can_be_deleted(self):
+        self.ensure_one()
+        return self.state == "draft"
+
+    def unlink(self):
+        for request in self:
+            if not request._can_be_deleted():
+                raise UserError(
+                    _("You cannot delete a purchase request which is not draft.")
+                )
+        return super(initial_data, self).unlink()
+
+    def button_draft(self):
+        self.mapped("line_ids").do_uncancel()
+        return self.write({"state": "draft"})
+
+    def button_to_approve(self):
+        self.to_approve_allowed_check()
+        return self.write({"state": "to_approve"})
+
+    def button_approved(self):
+        return self.write({"state": "approved"})
+
+    def button_rejected(self):
+        self.mapped("line_ids").do_cancel()
+        return self.write({"state": "rejected"})
+
+    def button_done(self):
+        return self.write({"state": "done"})
+
+    def check_auto_reject(self):
+        """When all lines are cancelled the purchase request should be
+        auto-rejected."""
+        for pr in self:
+            if not pr.line_ids.filtered(lambda l: l.cancelled is False):
+                pr.write({"state": "rejected"})
+    
+    def to_approve_allowed_check(self):
+        for rec in self:
+            if not rec.to_approve_allowed:
+                raise UserError(
+                    _(
+                        "You can't request an approval for a purchase request "
+                        "which is empty. (%s)"
+                    )
+                    % rec.name
+                )
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        if self.partner_id.property_account_receivable_id:
+            self.credit_account = self.partner_id.property_account_payable_id
+
+    @api.model
+    def _cron_generate_entries(self):
+        data = self.env['account.recurring.payments'].search(
+            [('state', '=', 'running')])
+        entries = self.env['account.move'].search(
+            [('recurring_ref', '!=', False)])
+        journal_dates = []
+        journal_codes = []
+        remaining_dates = []
+        for entry in entries:
+            journal_dates.append(str(entry.date))
+            if entry.recurring_ref:
+                journal_codes.append(str(entry.recurring_ref))
+        today = datetime.today()
+        for line in data:
+            if line.date:
+                recurr_dates = []
+                start_date = datetime.strptime(str(line.date), '%Y-%m-%d')
+                while start_date <= today:
+                    recurr_dates.append(str(start_date.date()))
+                    if line.recurring_period == 'days':
+                        start_date += relativedelta(
+                            days=line.recurring_interval)
+                    elif line.recurring_period == 'weeks':
+                        start_date += relativedelta(
+                            weeks=line.recurring_interval)
+                    elif line.recurring_period == 'months':
+                        start_date += relativedelta(
+                            months=line.recurring_interval)
+                    else:
+                        start_date += relativedelta(
+                            years=line.recurring_interval)
+                for rec in recurr_dates:
+                    recurr_code = str(line.id) + '/' + str(rec)
+                    if recurr_code not in journal_codes:
+                        remaining_dates.append({
+                            'date': rec,
+                            'template_name': line.name,
+                            'amount': line.amount,
+                            'tmpl_id': line.id,
+                        })
+        child_ids = self.recurring_lines.create(remaining_dates)
+        for line in child_ids:
+            tmpl_id = line.tmpl_id
+            recurr_code = str(tmpl_id.id) + '/' + str(line.date)
+            line_ids = [(0, 0, {
+                'account_id': tmpl_id.credit_account.id,
+                'partner_id': tmpl_id.partner_id.id,
+                'credit': line.amount,
+                # 'analytic_account_id': tmpl_id.analytic_account_id.id,
+            }), (0, 0, {
+                'account_id': tmpl_id.debit_account.id,
+                'partner_id': tmpl_id.partner_id.id,
+                'debit': line.amount,
+                # 'analytic_account_id': tmpl_id.analytic_account_id.id,
+            })]
+            vals = {
+                'date': line.date,
+                'recurring_ref': recurr_code,
+                'company_id': self.env.company.id,
+                'journal_id': tmpl_id.journal_id.id,
+                'ref': line.template_name,
+                'narration': 'Recurring entry',
+                'line_ids': line_ids
+            }
+            move_id = self.env['account.move'].create(vals)
+            if tmpl_id.journal_state == 'posted':
+                move_id.post()
     @api.model
     def preparar_objeto(self,linea):
         return {
