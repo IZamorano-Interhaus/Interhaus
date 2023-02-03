fa81b410ba530a445c41ebb650b0a42add3334c6
nicolas-interhaus <izamorano@interhaus.cl>
2023-02-03 10:40:19 -0300

    actualizacion 2.2, se actualiza las listas conlos distintos tipos de datos que arrojara

--- new_module/wizard/datos_iniciales.py
+++ new_module/wizard/datos_iniciales.py
@@ -16,18 +16,14 @@ _STATES = [
 class initial_data(models.TransientModel):
     _name='new_module.new_module'
     _description='new_module.new_module'
-    cliente = fields.Char(
-        string="Referencia comprador",
-        required=True,
-        default=lambda self: _("Ejemplo: Nicolas"),
-    )
+    cliente = fields.Many2one('res.partner', 'Partner')
     rut_tributario = fields.Char(
         string="Rut",
         required=True,
         default=lambda self: _("Sin puntos y con guión"),
     )   
     documento=fields.Many2one(
-        comodel_name="product.product",
+        comodel_name="account.account",
         string="documento",
         copy=False,
         index=True,
@@ -70,13 +66,13 @@ class initial_data(models.TransientModel):
         required=True,
     )
     diario = fields.Many2one(
-        comodel_name="procurement.group",
+        comodel_name="account.account",
         string="Diario",
         copy=False,
         index=True,
     )
     supplier_id = fields.Many2one(
-        comodel_name="purchase.order",
+        comodel_name="account.journal",
         string="Supplier",
         required=True,
         context={"res_partner_search_mode": "supplier"},
@@ -101,45 +97,7 @@ class initial_data(models.TransientModel):
                     start_date += relativedelta(months=self.recurring_interval)
                 else:
                     start_date += relativedelta(years=self.recurring_interval)
-            self.next_date = start_date.date()
-    name = fields.Char('Name')
-    debit_account = fields.Many2one('account.account', 'Debit Account',
-                                    required=True,
-                                    domain="['|', ('company_id', '=', False), "
-                                           "('company_id', '=', company_id)]")
-    credit_account = fields.Many2one('account.account', 'Credit Account',
-                                     required=True,
-                                     domain="['|', ('company_id', '=', False), "
-                                            "('company_id', '=', company_id)]")
-    journal_id = fields.Many2one('account.journal', 'Journal', required=True)
-    analytic_account_id = fields.Many2one('account.analytic.account',
-                                          'Analytic Account')
-    date = fields.Date('Starting Date', required=True, default=date.today())
-    next_date = fields.Date('Next Schedule', compute=_get_next_schedule,
-                            readonly=True, copy=False)
-    recurring_period = fields.Selection(selection=[('days', 'Days'),
-                                                   ('weeks', 'Weeks'),
-                                                   ('months', 'Months'),
-                                                   ('years', 'Years')],
-                                        store=True, required=True)
-    amount = fields.Float('Amount')
-    description = fields.Text('Description')
-    state = fields.Selection(selection=[('draft', 'Draft'),
-                                        ('running', 'Running')],
-                             default='draft', string='Status')
-    journal_state = fields.Selection(selection=[('draft', 'Unposted'),
-                                                ('posted', 'Posted')],
-                                     required=True, default='draft',
-                                     string='Generate Journal As')
-    recurring_interval = fields.Integer('Recurring Interval', default=1)
-    partner_id = fields.Many2one('res.partner', 'Partner')
-    pay_time = fields.Selection(selection=[('pay_now', 'Pay Directly'),
-                                           ('pay_later', 'Pay Later')],
-                                store=True, required=True)
-    company_id = fields.Many2one('res.company',
-                                 default=lambda l: l.env.company.id)
-    
-
+            self.next_date = start_date.date()   
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
