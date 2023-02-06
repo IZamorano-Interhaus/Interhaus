ad8ab8701bae169796c334e15fdb0ef3874f710b
nicolas-interhaus <izamorano@interhaus.cl>
2023-02-06 17:13:48 -0300

    actualizacion 1.4.8

--- new_module/wizard/datos_iniciales.py
+++ new_module/wizard/datos_iniciales.py
@@ -39,8 +39,7 @@ class PurchaseRequestLineMakePurchaseOrder(models.TransientModel):
     def _prepare_item(self, line):
         return {
             "line_id": line.id,
-            "product_id": line.product_id.id,
-            "name": line.name or line.product_id.name,
+            
             "product_qty": line.pending_qty_to_receive,
             "product_uom_id": line.product_uom_id.id,
         }
@@ -96,73 +95,9 @@ class PurchaseRequestLineMakePurchaseOrder(models.TransientModel):
         }
         return self.env["purchase.request.allocation"].create(vals)
 
-    @api.model
-    def _prepare_purchase_order_line(self, po, item):
-        if not item.product_id:
-            raise UserError(_("Please select a product for all lines"))
-        product = item.product_id
+    
 
-        # Keep the standard product UOM for purchase order so we should
-        # convert the product quantity to this UOM
-        qty = item.product_uom_id._compute_quantity(
-            item.product_qty, product.uom_po_id or product.uom_id
-        )
-        # Suggest the supplier min qty as it's done in Odoo core
-        min_qty = item.line_id._get_supplier_min_qty(product, po.partner_id)
-        qty = max(qty, min_qty)
-        date_required = item.line_id.date_required
-        return {
-            "order_id": po.id,
-            "product_id": product.id,
-            "product_uom": product.uom_po_id.id or product.uom_id.id,
-            "price_unit": 0.0,
-            "product_qty": qty,
-            "analytic_distribution": item.line_id.analytic_distribution,
-            "purchase_request_lines": [(4, item.line_id.id)],
-            "date_planned": datetime(
-                date_required.year, date_required.month, date_required.day
-            ),
-            "move_dest_ids": [(4, x.id) for x in item.line_id.move_dest_ids],
-        }
-
-    @api.model
-    def _get_purchase_line_name(self, order, line):
-        """Fetch the product name as per supplier settings"""
-        product_lang = line.product_id.with_context(
-            lang=get_lang(self.env, self.supplier_id.lang).code,
-            partner_id=self.supplier_id.id,
-            company_id=order.company_id.id,
-        )
-        name = product_lang.display_name
-        if product_lang.description_purchase:
-            name += "\n" + product_lang.description_purchase
-        return name
-
-    @api.model
-    def _get_order_line_search_domain(self, order, item):
-        vals = self._prepare_purchase_order_line(order, item)
-        name = self._get_purchase_line_name(order, item)
-        order_line_data = [
-            ("order_id", "=", order.id),
-            ("name", "=", name),
-            ("product_id", "=", item.product_id.id),
-            ("product_uom", "=", vals["product_uom"]),
-            ("analytic_distribution", "=?", item.line_id.analytic_distribution),
-        ]
-        if self.sync_data_planned:
-            date_required = item.line_id.date_required
-            order_line_data += [
-                (
-                    "date_planned",
-                    "=",
-                    datetime(
-                        date_required.year, date_required.month, date_required.day
-                    ),
-                )
-            ]
-        if not item.product_id:
-            order_line_data.append(("name", "=", item.name))
-        return order_line_data
+    
 
     
 
@@ -182,12 +117,7 @@ class PurchaseRequestLineMakePurchaseOrderItem(models.TransientModel):
         comodel_name="purchase.request.line", string="Purchase Request Line"
     )
    
-    product_id = fields.Many2one(
-        comodel_name="product.product",
-        string="Product",
-        related="line_id.product_id",
-        readonly=False,
-    )
+    
     name = fields.Char(string="Description", required=True)
     product_qty = fields.Float(
         string="Quantity to purchase", digits="Product Unit of Measure"
@@ -202,33 +132,4 @@ class PurchaseRequestLineMakePurchaseOrderItem(models.TransientModel):
         "wizard in the new PO.",
     )
 
-    @api.onchange("product_id")
-    def onchange_product_id(self):
-        if self.product_id:
-            if not self.keep_description:
-                name = self.product_id.name
-            code = self.product_id.code
-            sup_info_id = self.env["product.supplierinfo"].search(
-                [
-                    "|",
-                    ("product_id", "=", self.product_id.id),
-                    ("product_tmpl_id", "=", self.product_id.product_tmpl_id.id),
-                    ("partner_id", "=", self.wiz_id.supplier_id.id),
-                ]
-            )
-            if sup_info_id:
-                p_code = sup_info_id[0].product_code
-                p_name = sup_info_id[0].product_name
-                name = "[{}] {}".format(
-                    p_code if p_code else code, p_name if p_name else name
-                )
-            else:
-                if code:
-                    name = "[{}] {}".format(
-                        code, self.name if self.keep_description else name
-                    )
-            if self.product_id.description_purchase and not self.keep_description:
-                name += "\n" + self.product_id.description_purchase
-            self.product_uom_id = self.product_id.uom_id.id
-            if name:
-                self.name = name
+    
