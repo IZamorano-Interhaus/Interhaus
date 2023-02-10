6581adeec53a6dc24821dc727af9836b48a6a8f7
nicolas-interhaus <izamorano@interhaus.cl>
2023-02-10 16:24:10 -0300

    cambio 10/02/2023 cambio 2.2

--- new_module/models/import_data.py
+++ new_module/models/import_data.py
@@ -1,6 +1,18 @@
-import psycopg2,os
+import os
 os.system('clear')
 
-con = psycopg2.connect(database="nicolas", user="nicolas", password="", host="localhost", port="5432")
+import psycopg2
 
-print("Database opened successfully")
+conexion1 = psycopg2.connect(database="interhuas_testing2", user="postgres", password="")
+cursor1=conexion1.cursor()
+createTable= "create table articulos(codigo serial, descripcion varchar(40),precio float,primary key (codigo));"
+cursor1.execute(createTable)
+sql="insert into articulos(descripcion, precio) values (%s,%s)"
+datos=("naranjas", 23.50)
+cursor1.execute(sql, datos)
+datos=("peras", 34)
+cursor1.execute(sql, datos)
+datos=("bananas", 25)
+cursor1.execute(sql, datos)
+conexion1.commit()
+conexion1.close() 
