b4d7c7ea4f29e6d32e02e7d146ad2774ba840bc2
nicolas-interhaus <izamorano@interhaus.cl>
2023-02-13 11:04:05 -0300

    cambio 1.3

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import json
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 from datetime import datetime, date
@@ -70,16 +71,57 @@ class new_module(models.Model):
         # return record
 
 
-    """ @api.model
-    def getDocument(self):
+    @api.model
+    def getDocument(self, *post):
         with open("new 2.json") as archivo:
             auxdiccionario = json.load(archivo)
         with open("new 2.json", 'w') as archivo_nuevo:
             json.dump(auxdiccionario, archivo_nuevo)
+            company_id = self.get_current_company_value()
+
+        states_arg = ""
+        if post != ('posted',):
+            states_arg = """ account_move.state in ('posted', 'draft')"""
+        else:
+            states_arg = """ account_move.state = 'posted'"""
+
+        self._cr.execute(('''  select res_partner.name as partner, res_partner.commercial_partner_id as res  ,
+                            account_move.commercial_partner_id as parent, sum(account_move.amount_total) as amount
+                            from account_move,res_partner where 
+                            account_move.partner_id=res_partner.id AND account_move.move_type = 'in_invoice' AND
+                            payment_state = 'not_paid' AND 
+                              account_move.company_id in ''' + str(tuple(company_id)) + ''' AND
+                            %s 
+                            AND  account_move.commercial_partner_id=res_partner.commercial_partner_id 
+                            group by parent,partner,res
+                            order by amount desc ''') % (states_arg))
+
+        record = self._cr.dictfetchall()
+
+        bill_partner = [item['partner'] for item in record]
+
+        bill_amount = [item['amount'] for item in record]
+
+        amounts = sum(bill_amount[9:])
+        name = bill_partner[9:]
+        results = []
+        pre_partner = []
+
+        bill_amount = bill_amount[:9]
+        bill_amount.append(amounts)
+        bill_partner = bill_partner[:9]
+        bill_partner.append("Others")
+        records = {
+            'bill_partner': bill_partner,
+            'bill_amount': bill_amount,
+            'result': results,
+
+        }
+        return records
         for x in range(len(auxdiccionario["ventas"]["detalleVentas"])):
             if x.folio_documento=="":
                 numero+=1
                 auxlista.append(auxdiccionario["ventas"]['detalleVentas'][0+(numero-1)]["rutCliente"]+" | "+str(auxdiccionario["ventas"]['detalleVentas'][0+(numero-1)]["folio"]))
                 self._cr.execute(
 
-                ) """
+                )
