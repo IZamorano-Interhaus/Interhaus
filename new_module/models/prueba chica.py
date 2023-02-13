a3811eb6b7ce3dad3515437d623dadfb44caf292
nicolas-interhaus <izamorano@interhaus.cl>
2023-02-13 10:37:30 -0300

    cambio 1.2.5

--- new_module/models/prueba chica.py
+++ new_module/models/prueba chica.py
@@ -1,6 +1,5 @@
-import json,os
-os.system('clear')
-auxlista=list()
+import json
+auxlista=[]
 numero=0
 with open("/home/nicolas/Documentos/new 2.json") as archivo:
     auxdiccionario = json.load(archivo)
@@ -8,7 +7,7 @@ with open("/home/nicolas/Documentos/new 2.json", 'w') as archivo_nuevo:
     json.dump(auxdiccionario, archivo_nuevo)
 for x in range(len(auxdiccionario["ventas"]["detalleVentas"])):
     numero+=1
-    auxlista.append(auxdiccionario["ventas"]['detalleVentas'][0+(numero-1)]["rutCliente"]+" | "+str(auxdiccionario["ventas"]['detalleVentas'][0+(numero-1)]["folio"]))
+    auxlista.append(auxdiccionario["ventas"]['detalleVentas'][0+(numero-1)]["rutCliente"]+str(auxdiccionario["ventas"]['detalleVentas'][0+(numero-1)]["folio"]))
 print (auxlista)
 """ def _build_comparison_context(self, data):
         result = {}
@@ -21,63 +20,6 @@ print (auxlista)
             result['date_to'] = data['form']['date_to_cmp']
             result['strict_range'] = True
         return result """
-
-
-""" def _build_contexts(self, data):
-        result = {}
-        result['journal_ids'] = 'journal_ids' in data['form'] and data['form']['journal_ids'] or False
-        result['state'] = 'target_move' in data['form'] and data['form']['target_move'] or ''
-        result['date_from'] = data['form']['date_from'] or False
-        result['date_to'] = data['form']['date_to'] or False
-        result['strict_range'] = True if result['date_from'] else False
-        result['company_id'] = data['form']['company_id'][0] or False
-        return result """
-
-""" def find_journal_items(self, report_lines, form):
-        cr = self.env.cr
-        journal_items = []
-        for i in report_lines:
-            if i['type'] == 'account':
-                account = i['account']
-                if form['target_move'] == 'posted':
-                    search_query = "select aml.id, am.id as j_id, aml.account_id, aml.date," \
-                                   " aml.name as label, am.name, " \
-                                   + "(aml.debit-aml.credit) as balance, aml.debit, aml.credit, aml.partner_id " \
-                                   + " from account_move_line aml join account_move am " \
-                                     "on (aml.move_id=am.id and am.state=%s) " \
-                                   + " where aml.account_id=%s"
-                    vals = [form['target_move']]
-                else:
-                    search_query = "select aml.id, am.id as j_id, aml.account_id, aml.date, " \
-                                   "aml.name as label, am.name, " \
-                                   + "(aml.debit-aml.credit) as balance, aml.debit, aml.credit, aml.partner_id " \
-                                   + " from account_move_line aml join account_move am on (aml.move_id=am.id) " \
-                                   + " where aml.account_id=%s"
-                    vals = []
-                if form['date_from'] and form['date_to']:
-                    search_query += " and aml.date>=%s and aml.date<=%s"
-                    vals += [account, form['date_from'], form['date_to']]
-                elif form['date_from']:
-                    search_query += " and aml.date>=%s"
-                    vals += [account, form['date_from']]
-                elif form['date_to']:
-                    search_query += " and aml.date<=%s"
-                    vals += [account, form['date_to']]
-                else:
-                    vals += [account]
-                cr.execute(search_query, tuple(vals))
-                items = cr.dictfetchall()
-
-                for j in items:
-                    temp = j['id']
-                    j['id'] = re.sub('[^0-9a-zA-Z]+', '', i['name']) + str(
-                        temp)
-                    j['p_id'] = str(i['a_id'])
-                    j['type'] = 'journal_item'
-                    journal_items.append(j)
-        return journal_items """
-
-
 """ def _build_contexts(self, data):
         result = {}
         result['journal_ids'] = 'journal_ids' in data['form'] and data['form'][
