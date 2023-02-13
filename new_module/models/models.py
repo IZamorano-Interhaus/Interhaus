df0324b3135f02eb9b050197d053f731d910cb55
nicolas-interhaus <izamorano@interhaus.cl>
2023-02-13 15:03:45 -0300

    cambio 1.5

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -121,6 +121,65 @@ class new_module(models.Model):
 
         }
         return records
+
+    @api.model
+    def get_latebillss(self, *post):
+        company_id = self.get_current_company_value()
+
+        partners = self.env['res.partner'].search([('active', '=', True)])
+
+        states_arg = ""
+        if post[0] != 'posted':
+            states_arg = """ account_move.state in ('posted', 'draft')"""
+        else:
+            states_arg = """ account_move.state = 'posted'"""
+
+        if post[1] == 'this_month':
+            self._cr.execute((''' 
+                                select to_char(account_move.date, 'Month') as month, res_partner.name as bill_partner, account_move.partner_id as parent,
+                                sum(account_move.amount_total) as amount from account_move, res_partner where account_move.partner_id = res_partner.id
+                                AND account_move.move_type = 'in_invoice'
+                                AND payment_state = 'not_paid'
+                                AND %s 
+                                AND Extract(month FROM account_move.invoice_date_due) = Extract(month FROM DATE(NOW()))
+                                AND Extract(YEAR FROM account_move.invoice_date_due) = Extract(YEAR FROM DATE(NOW()))
+                                AND account_move.company_id in ''' + str(tuple(company_id)) + '''
+                                AND account_move.partner_id = res_partner.commercial_partner_id
+                                group by parent, bill_partner, month
+                                order by amount desc ''') % (states_arg))
+        else:
+            self._cr.execute((''' select res_partner.name as bill_partner, account_move.partner_id as parent,
+                                            sum(account_move.amount_total) as amount from account_move, res_partner where account_move.partner_id = res_partner.id
+                                            AND account_move.move_type = 'in_invoice'
+                                            AND payment_state = 'not_paid'
+                                            AND %s
+                                            AND Extract(YEAR FROM account_move.invoice_date_due) = Extract(YEAR FROM DATE(NOW()))
+                                            AND account_move.partner_id = res_partner.commercial_partner_id
+                                            AND account_move.company_id in ''' + str(tuple(company_id)) + '''
+                                            group by parent, bill_partner
+                                            order by amount desc ''') % (states_arg))
+
+        result = self._cr.dictfetchall()
+        bill_partner = [item['bill_partner'] for item in result]
+
+        bill_amount = [item['amount'] for item in result]
+
+        amounts = sum(bill_amount[9:])
+        name = bill_partner[9:]
+        results = []
+        pre_partner = []
+
+        bill_amount = bill_amount[:9]
+        bill_amount.append(amounts)
+        bill_partner = bill_partner[:9]
+        bill_partner.append("Others")
+        records = {
+            'bill_partner': bill_partner,
+            'bill_amount': bill_amount,
+            'result': results,
+
+        }
+        return records
         """ for x in range(len(auxdiccionario["ventas"]["detalleVentas"])):
             if x.folio_documento=="":
                 numero+=1
