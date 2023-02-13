d3036421535d961e2d0c34f97648c637bc2ad2fe
nicolas-interhaus <izamorano@interhaus.cl>
2023-02-13 11:42:10 -0300

    cambio 1.3.1

--- new_module/models/prueba chica.py
+++ new_module/models/prueba chica.py
@@ -9,24 +9,3 @@ for x in range(len(auxdiccionario["ventas"]["detalleVentas"])):
     numero+=1
     auxlista.append(auxdiccionario["ventas"]['detalleVentas'][0+(numero-1)]["rutCliente"]+str(auxdiccionario["ventas"]['detalleVentas'][0+(numero-1)]["folio"]))
 print (auxlista)
-""" def _build_comparison_context(self, data):
-        result = {}
-        result['journal_ids'] = 'journal_ids' in data['form'] and data['form'][
-            'journal_ids'] or False
-        result['state'] = 'target_move' in data['form'] and data['form'][
-            'target_move'] or ''
-        if data['form']['filter_cmp'] == 'filter_date':
-            result['date_from'] = data['form']['date_from_cmp']
-            result['date_to'] = data['form']['date_to_cmp']
-            result['strict_range'] = True
-        return result """
-""" def _build_contexts(self, data):
-        result = {}
-        result['journal_ids'] = 'journal_ids' in data['form'] and data['form'][
-            'journal_ids'] or False
-        result['state'] = 'target_move' in data['form'] and data['form'][
-            'target_move'] or ''
-        result['date_from'] = data['form']['date_from'] or False
-        result['date_to'] = data['form']['date_to'] or False
-        result['strict_range'] = True if result['date_from'] else False
-        return result """
