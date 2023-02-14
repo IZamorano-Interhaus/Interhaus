f6acb7c39f0e2b7eac91ba7f1f5b5aff2ff1599b
nicolas-interhaus <izamorano@interhaus.cl>
2023-02-14 09:42:17 -0300

    cambio 10.2.3

--- new_module/models/import_data.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models, _
-from odoo import tools
-from dateutil.relativedelta import relativedelta
-from odoo import models, api
-from odoo.http import request
-
