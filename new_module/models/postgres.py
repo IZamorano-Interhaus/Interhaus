9fcf3d3e42a578fb0b15630bc4c19bc007b20c92
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-02-16 12:18:28 -0300

    cambio del dia

---
+++ new_module/models/postgres.py
@@ -0,0 +1,25 @@
+
+import psycopg2
+
+conn = psycopg2.connect(database="interhaus_testing2", user = "postgres", password = "admin", host = "localhost", port = "5432")
+
+cur = conn.cursor()
+
+query = "select * from documentos;"
+cur.execute("INSERT INTO documentos (ID,rut,folio) \
+      VALUES (1, '196694684', 32 );");
+
+cur.execute("INSERT INTO documentos (ID,rut,folio) \
+      VALUES (2, '72893782-2', 25);");
+
+cur.execute("INSERT INTO documentos (ID,rut,folio) \
+      VALUES (3, '18679478-7', 23);");
+
+cur.execute("INSERT INTO documentos (ID,rut,folio) \
+      VALUES (4, '10709562-4', 25);");
+
+cur.execute(query)
+conn.commit()
+print ("Records created successfully");
+conn.close()
+
