05737a76091fd67c065668771f36c28907db99fd
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-02-16 20:13:18 -0300

    archivos python que inserta y llama a los datos de la tabla, se realizara la comparacion

--- new_module/models/prueba chica.py
+++ new_module/models/prueba chica.py
@@ -9,31 +9,19 @@ with open("C:/tools/respaldoBaseDatos/SIIpruebas.json", 'w') as archivo_nuevo:
 for x in range(len(auxdiccionario["ventas"]["detalleVentas"])):
     numero+=1
     auxlista.append(auxdiccionario["ventas"]['detalleVentas'][0+(numero-1)]["rutCliente"]+str(auxdiccionario["ventas"]['detalleVentas'][0+(numero-1)]["folio"]))
-print (auxlista)
 conn = psycopg2.connect(database="testing", user = "postgres", password = "admin", host = "localhost", port = "5432")
 
 cur = conn.cursor()
 
 query = "select * from documentos;"
-cur.execute("INSERT INTO documentos (ID,rut,folio) \
-      VALUES (1, '196694684', 32 );");
 
-cur.execute("INSERT INTO documentos (ID,rut,folio) \
-      VALUES (2, '72893782-2', 25);");
-
-cur.execute("INSERT INTO documentos (ID,rut,folio) \
-      VALUES (3, '18679478-7', 23);");
-
-cur.execute("INSERT INTO documentos (ID,rut,folio) \
-      VALUES (4, '10709562-4', 25);");
 
 cur.execute(query)
 rows = cur.fetchall()
-for row in rows:
-   print ("ID = ", row[0])
-   print ("rut = ", row[1])
-   print ("folio = ", row[2], "\n")
-
+for odoo in auxlista:
+    for columnas in rows:
+        if odoo[0]!=columnas[1]:
+            print("ejecucion")
 conn.commit()
 print ("Records created successfully");
 conn.close()
