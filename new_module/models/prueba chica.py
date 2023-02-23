3621495033197c0ba08ddf82f0fd4482817e965d
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-02-23 09:47:52 -0300

    que hacer con los datos que estan y no estan en los multiples arreglos

--- new_module/models/prueba chica.py
+++ new_module/models/prueba chica.py
@@ -15,7 +15,6 @@ for ingreso in (datosJSON["ventas"]["detalleVentas"]):
     lax=[]
     lax= ingreso["tipoDTEString"],str(ingreso["tipoDTE"]),ingreso["tipoCompra"],ingreso["rutCliente"],ingreso["razonSocial"],str(ingreso["folio"]),ingreso["fechaEmision"],ingreso["fechaRecepcion"],str(ingreso["montoExento"]),str(ingreso["montoNeto"]),str(ingreso["montoIvaRecuperable"]),str(ingreso["montoTotal"]),str(ingreso["ivaNoRetenido"]),str(ingreso["totalOtrosImpuestos"]),str(ingreso["valorOtroImpuesto"]),str(ingreso["tasaOtroImpuesto"]), str(ingreso["tipoDocReferencia"]),str(ingreso["trackId"]),ingreso["referencias"],ingreso["referenciado"],ingreso["reparos"],ingreso["otrosImpuestos"],ingreso["estado"]
     listaRut.append(lax)
-print(listaRut)
 cur.execute("drop table if exists borradores;")
 cur.execute("create table borradores (id serial, tipoDTEstring VARCHAR(50),tipoDTE int,tipoCompra VARCHAR(50),rutCliente VARCHAR(50),razonSocial VARCHAR(50),folio int,fechaEmision date,fechaRecepcion date,acuseRecibo VARCHAR(50),montoExento int,montoNeto int,montoIvaRecuperable float,montoTotal         float,ivaNoRetenido      float, totalOtrosImpuestos float,valorOtroImpuesto  float,tasaOtroImpuesto   float,tipoDocReferencia int,folioDocReferencia int,trackId int,referencias VARCHAR(50),referenciado VARCHAR(50),reparos VARCHAR(50),otrosImpuestos VARCHAR(50),estado VARCHAR(50)); ")
 query = "select * from borradores;"
@@ -26,21 +25,15 @@ for i in range(len(listaRut)):
     if range(len(querySelect))==0:
         cur.execute("insert into borradores (rutCliente,folio)values('61002000-3',5617);")
         cur.execute("select * from borradores;")
-        print(i)
-        print("primer insert si la lista no tiene datos")
     elif range(len(querySelect))!=0: 
-        print("entramos a la condicion si existe datos en queryselect")
-        print(cur.execute("select * from borradores;"))
+        
         for j in range(len(querySelect)):
             if listaRut[i]["rutCliente"]!=querySelect[j]["rut"] and listaRut[i]["folio"]!=querySelect[j]["folio"]:
-                print("entramos a la condicion si ")
                 l.append(listaRut[3]["rutCliente"]+listaRut[5]["folio"])
-                print(l)
         existe=True
     if existe:
-        print(cur.execute("select * from borradores;"))
-        print(range(len(querySelect)))
-        cur.execute("insert into borradores (rutCliente,folio) values('"+str(listaRut[i][3])+"',"+str(listaRut[i][5])+");")
+        cur.execute("insert into borradores (tipoDTEstring,tipoDTE,tipoCompra,rutCliente,razonSocial,folio,fechaEmision,fechaRecepcion,acuseRecibo) values('"+str(listaRut[i][0])+"',"+str(listaRut[i][1])+",'"+str(listaRut[i][2])+"','"+str(listaRut[i][3])+"','"+str(listaRut[i][4])+"',"+str(listaRut[i][5])+",'"+str(listaRut[i][6])+"','"+str(listaRut[i][7])+"','"+str(listaRut[i][8])+"');")
+print(l)
 conn.commit()
 print ("Records created successfully");
 conn.close()
