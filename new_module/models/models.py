bca429ddbf4bddc9c7b1e4240210e9de9e1a25b5
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-02-24 16:06:06 -0300

    agregando codigo al modulo

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import psycopg2,json,os,sys
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 from datetime import datetime, date
@@ -90,39 +91,48 @@ class new_module(models.Model):
                 adr_pref=['invoice']).get('invoice', rec.partner_id.id)
     @api.model
     def get_latebills(self, *post):
-        company_id = self.get_current_company_value()
-        states_arg = ""
-        if post != ('posted',):
-            states_arg = """ account_move.state in ('posted', 'draft')"""
-        else:
-            states_arg = """ account_move.state = 'posted'"""
-        self._cr.execute(('''  select res_partner.name as partner, res_partner.commercial_partner_id as res  ,
-                            account_move.commercial_partner_id as parent, sum(account_move.amount_total) as amount
-                            from account_move,res_partner where 
-                            account_move.partner_id=res_partner.id AND account_move.move_type = 'in_invoice' AND
-                            payment_state = 'not_paid' AND 
-                              account_move.company_id in ''' + str(tuple(company_id)) + ''' AND
-                            %s 
-                            AND  account_move.commercial_partner_id=res_partner.commercial_partner_id 
-                            group by parent,partner,res
-                            order by amount desc ''') % (states_arg))
-        record = self._cr.dictfetchall()
-        bill_partner = [item['partner'] for item in record]
-        bill_amount = [item['amount'] for item in record]
-        amounts = sum(bill_amount[9:])
-        name = bill_partner[9:]
-        results = []
-        pre_partner = []
-        bill_amount = bill_amount[:9]
-        bill_amount.append(amounts)
-        bill_partner = bill_partner[:9]
-        bill_partner.append("Others")
-        records = {
-            'bill_partner': bill_partner,
-            'bill_amount': bill_amount,
-            'result': results,
-        }
-        return records
-        # return record
-    # function to getting over dues
-   
+    
+        os.system('cls')
+        listaRut=[]
+        
+        conn = psycopg2.connect(database="testing", user = "postgres", password = "admin", host = "localhost", port = "5432")
+        cur = conn.cursor()
+        f = open("C:\tools\respaldoBaseDatos\doc_SII_202301", "r")
+        archivoJSON = f.read()
+        datosJSON = json.loads(archivoJSON)
+        for ingreso in (datosJSON["ventas"]["detalleVentas"]):
+            lax=[]
+            try:
+                if ingreso["acuseRecibo"]!=None:
+                    acuse=ingreso["acuseRecibo"]
+            except:
+                acuse=""
+            lax= ingreso["tipoDTEString"],str(ingreso["tipoDTE"]),ingreso["tipoCompra"],ingreso["rutCliente"],ingreso["razonSocial"],str(ingreso["folio"]),ingreso["fechaEmision"],ingreso["fechaRecepcion"],str(acuse),str(ingreso["montoExento"]),str(ingreso["montoNeto"]),str(ingreso["montoIvaRecuperable"]),str(ingreso["montoTotal"]),str(ingreso["ivaNoRetenido"]),str(ingreso["totalOtrosImpuestos"]),str(ingreso["valorOtroImpuesto"]),str(ingreso["tasaOtroImpuesto"]), str(ingreso["tipoDocReferencia"]),str(ingreso["trackId"]),ingreso["referencias"],ingreso["referenciado"],ingreso["reparos"],ingreso["otrosImpuestos"],ingreso["estado"]
+            listaRut.append(lax)
+        query = "select rutCliente,folio from borradores;" 
+        cur.execute(query)
+        querySelect = cur.fetchall()
+        largoQuery=len(querySelect)
+        for i in range(len(listaRut)): 
+            existe=True
+            if largoQuery==0:
+                existe=False
+            else: 
+                existe=True
+                for j in range(largoQuery):
+                    if str(listaRut[i][3])==str(querySelect[j][0]) and str(listaRut[i][5])==str(querySelect[j][1]):
+                        existe=True
+                        break
+                    else:
+                        existe=False
+            if existe==False:
+                cur.execute("insert into borradores (tipoDTEstring,tipoDTE,tipoCompra,rutCliente,razonSocial,folio,fechaEmision,fechaRecepcion,acuseRecibo,montoExento,montoNeto,montoIvaRecuperable,montoTotal,ivaNoRetenido,totalOtrosImpuestos,valorOtroImpuesto,tasaOtroImpuesto,tipoDocReferencia,trackId,referencias,referenciado,reparos,otrosImpuestos,estado) values('"+str(listaRut[i][0])+"',"+str(listaRut[i][1])+",'"+str(listaRut[i][2])+"','"+str(listaRut[i][3])+"','"+str(listaRut[i][4])+"',"+str(listaRut[i][5])+",'"+str(listaRut[i][6])+"','"+str(listaRut[i][7])+"','"+str(listaRut[i][8])+"','"+str(listaRut[i][9])+"','"+str(listaRut[i][10])+"','"+str(listaRut[i][11])+"','"+str(listaRut[i][12])+"','"+str(listaRut[i][13])+"','"+str(listaRut[i][14])+"','"+str(listaRut[i][15])+"','"+str(listaRut[i][16])+"','"+str(listaRut[i][17])+"','"+str(listaRut[i][18])+"','"+str(listaRut[i][19])+"','"+str(listaRut[i][20])+"','"+str(listaRut[i][21])+"','"+str(listaRut[i][22])+"','"+str(listaRut[i][23])+"');")
+                cur.execute(query)
+                querySelect = cur.fetchall()
+                largoQuery=len(querySelect)
+                print("query despues del ciclo parte 2 => "+str(largoQuery))
+        conn.commit()
+        print("script completado")
+        conn.close()
+            # return record
+        # function to getting over dues
