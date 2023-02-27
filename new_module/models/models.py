f70a8a2fee6820e21f6b786c0f30894f185dc0b7
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-02-27 16:07:24 -0300

    cambio en el views 2.2.6

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -4,15 +4,6 @@ from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 from datetime import datetime, date
 from dateutil.relativedelta import relativedelta
-_STATES = [
-    ("draft", "Draft"),
-    ("to_approve", "To be approved"),
-    ("approved", "Approved"),
-    ("rejected", "Rejected"),
-    ("done", "Done"),
-]
-auxlista=list()
-numero=0
 class new_module(models.Model):
     _name = 'new_module.new_module'
     _description = 'new_module.new_module' 
