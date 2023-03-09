25390222b9903da59c8cee150f2ad3aecfcdead2
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-03-09 14:45:58 -0300

    se agrega funcion insertar proveedores

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -186,9 +186,14 @@ class new_module(models.Model):
             # return record
         # function to getting over dues
     def obtenerDataProveedor():
-        archivo_xml= open('C:/Users/Interhouse HP/Desktop/zonaTesting/testeos/zonaPython/xmlPruebas/prueba4.xml')
-        datosProveedor = []
+        import xml.etree.ElementTree as ET
+        import psycopg2, os
         try:
+            archivo_xml= open('C:/Users/Interhouse HP/Desktop/zonaTesting/testeos/zonaPython/xmlPruebas/prueba5.xml')
+            os.system('cls')
+            datosProveedor = []
+            conn = psycopg2.connect(database="testing", user = "postgres", password = "admin", host = "localhost", port = "5432")
+            cur = conn.cursor()
             if archivo_xml.readable:
                 dato_xml = ET.fromstring(archivo_xml.read())
                 nodoSetDTE = dato_xml.find('{http://www.sii.cl/SiiDte}SetDTE')
@@ -197,36 +202,27 @@ class new_module(models.Model):
                 nodoEncabezado = nodoDocumento.find('{http://www.sii.cl/SiiDte}Encabezado') 
                 nodoEmisor = nodoEncabezado.find('{http://www.sii.cl/SiiDte}Emisor')
                 datosProveedor=nodoEmisor.find('{http://www.sii.cl/SiiDte}RUTEmisor').text,nodoEmisor.find('{http://www.sii.cl/SiiDte}DirOrigen').text,nodoEmisor.find('{http://www.sii.cl/SiiDte}CmnaOrigen').text,nodoEmisor.find('{http://www.sii.cl/SiiDte}RznSoc').text,nodoEmisor.find('{http://www.sii.cl/SiiDte}GiroEmis').text
-                print(datosProveedor)
-                conn = psycopg2.connect(database="testing", user = "postgres", password = "admin", host = "localhost", port = "5432")
-                cur = conn.cursor()
-                query = "select vat,name, display_name,street,city,l10n_cl_activity_description from res_partner;" 
+                query = "select vat from res_partner;" 
                 cur.execute(query)
                 querySelect = cur.fetchall()
-                largoQuery=len(querySelect)
-                for i in range(len(datosProveedor)): 
+                largoQuery=len(querySelect)        
+                existe=True
+                if largoQuery==0:
+                    existe=False
+                else: 
                     existe=True
-                    if largoQuery==0:
-                        existe=False
-                    else: 
-                        existe=True
-                        for j in range(largoQuery):
-                            if str(datosProveedor[i][0])==str(querySelect[j][0]):
-                                existe=True
-                                break
-                            else:
-                                existe=False
-                    if existe==False:
-                        cur.execute("insert into res_partner(vat,name, display_name,street,city,l10n_cl_activity_description) values();")
-                        cur.execute(query)
-                        querySelect = cur.fetchall()
-                        largoQuery=len(querySelect)
-                        print("query despues del ciclo parte 2 => "+str(largoQuery))
-                
-
+                    for j in range(largoQuery):
+                        if str(datosProveedor[0])==str(querySelect[j][0]):
+                            existe=True
+                            break
+                        else:
+                            existe=False
+                if existe==False:
+                    cur.execute("insert into res_partner (id,vat, street, city,display_name ,l10n_cl_activity_description) values (1,'"+datosProveedor[0]+"','"+datosProveedor[1]+"','"+datosProveedor[2]+"','"+datosProveedor[3]+"','"+datosProveedor[4]+"');")
+                    cur.execute(query)
+                    querySelect = cur.fetchall()
+                    largoQuery=len(querySelect)
                 conn.commit()
-
-                print("script completado")
                 conn.close()
             else:
                 print(False)
