6d599772a87654c752eef5fda2fdfd7c462e16cc
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-03-10 12:18:12 -0300

    se agrega el modulo contact de la fuente de odoo

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -189,8 +189,8 @@ class new_module(models.Model):
         import xml.etree.ElementTree as ET
         import psycopg2, os
         try:
-            archivo_xml= open('C:/Users/Interhouse HP/Desktop/zonaTesting/testeos/zonaPython/xmlPruebas/prueba5.xml')
-            os.system('cls')
+            archivo_xml= open('C:/Users/Interhouse HP/Desktop/zonaTesting/testeos/zonaPython/xmlPruebas/prueba1.xml')
+            
             datosProveedor = []
             conn = psycopg2.connect(database="testing", user = "postgres", password = "admin", host = "localhost", port = "5432")
             cur = conn.cursor()
@@ -202,27 +202,30 @@ class new_module(models.Model):
                 nodoEncabezado = nodoDocumento.find('{http://www.sii.cl/SiiDte}Encabezado') 
                 nodoEmisor = nodoEncabezado.find('{http://www.sii.cl/SiiDte}Emisor')
                 datosProveedor=nodoEmisor.find('{http://www.sii.cl/SiiDte}RUTEmisor').text,nodoEmisor.find('{http://www.sii.cl/SiiDte}DirOrigen').text,nodoEmisor.find('{http://www.sii.cl/SiiDte}CmnaOrigen').text,nodoEmisor.find('{http://www.sii.cl/SiiDte}RznSoc').text,nodoEmisor.find('{http://www.sii.cl/SiiDte}GiroEmis').text
+                print(datosProveedor)
                 query = "select vat from res_partner;" 
                 cur.execute(query)
                 querySelect = cur.fetchall()
                 largoQuery=len(querySelect)        
                 existe=True
-                if largoQuery==0:
-                    existe=False
-                else: 
-                    existe=True
-                    for j in range(largoQuery):
-                        if str(datosProveedor[0])==str(querySelect[j][0]):
-                            existe=True
-                            cur.execute("update res_partner set (vat="+datosProveedor[0]+", street="+datosProveedor[1]+", city="+datosProveedor[2]+",display_name ="+datosProveedor[3]+",l10n_cl_activity_description="+datosProveedor[4]+")")
-                        else:
-                            existe=False
-                if existe==False:
-                    cur.execute("insert into res_partner (id,vat, street, city,display_name ,l10n_cl_activity_description) values (1,'"+datosProveedor[0]+"','"+datosProveedor[1]+"','"+datosProveedor[2]+"','"+datosProveedor[3]+"','"+datosProveedor[4]+"');")
-                    cur.execute(query)
-                    querySelect = cur.fetchall()
-                    largoQuery=len(querySelect)
-                conn.commit()
+                for i in datosProveedor:
+                    if largoQuery==0:
+                        existe=False
+                    else: 
+                        existe=True
+                        for j in range(largoQuery):
+                            if str(datosProveedor[0])==str(querySelect[j][0]):
+                                existe=True
+                                cur.execute("update res_partner set vat='"+datosProveedor[0]+"', street='"+datosProveedor[1]+"', city='"+datosProveedor[2]+"',display_name ='"+datosProveedor[3]+"',name='"+datosProveedor[3]+"',l10n_cl_activity_description ='"+datosProveedor[4]+"' where vat='"+datosProveedor[0]+"';")
+                                break
+                            else:
+                                existe=False
+                    if existe==False:
+                        cur.execute("insert into res_partner (id,vat, street, city,name,display_name ,l10n_cl_activity_description) values (1,'"+datosProveedor[0]+"','"+datosProveedor[1]+"','"+datosProveedor[2]+"','"+datosProveedor[3]+"','"+datosProveedor[3]+"','"+datosProveedor[4]+"');")
+                        cur.execute(query)
+                        querySelect = cur.fetchall()
+                        largoQuery=len(querySelect)
+                    conn.commit()
                 conn.close()
             else:
                 print(False)
