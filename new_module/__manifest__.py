00f66e51ffe6f6050aca9f6a39b542dfdc586649
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-03-20 18:01:12 -0300

    nueva vista llamada vistazo

--- new_module/__manifest__.py
+++ new_module/__manifest__.py
@@ -25,7 +25,7 @@
     
         'security/ir.model.access.csv',
         'views/views.xml',
-        'views/vistzo.xml',
+        'views/vistazo.xml',
         'views/templates.xml'
         
     ],
