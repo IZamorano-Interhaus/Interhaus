2aa6d930ee476dff4296c781c4177f00664d4c69
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-03-20 16:26:25 -0300

    agregando parte a la seccion de pruebas

--- new_module/controllers/controllers.py
+++ new_module/controllers/controllers.py
@@ -11,10 +11,10 @@ class NewModule(http.Controller):
     def list(self, **kw):
         return http.request.render('new_module.listing', {
             'root': '/new_module/new_module',
-            'objects': http.request.env['new_module.new_module'].search([]),
+            'objects': http.request.env['new_module.pruebas'].search([]),
         })
 
-    @http.route('/new_module/new_module/objects/<model("new_module.new_module"):obj>', auth='public')
+    @http.route('/new_module/new_module/objects/<model("new_module.pruebas"):obj>', auth='public')
     def object(self, obj, **kw):
         return http.request.render('new_module.object', {
             'object': obj
