3d2667a11c58972123db265dbba2de34c48e11ed
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-03-29 16:45:12 -0300

    se actualiza la descripcion de actividad en el demo

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -202,7 +202,7 @@ class proveedores(models.Model):
         default='1', string = 'Tipo de contribuyente'
     )
     l10n_cl_sii_activity_description_id= fields.Many2one(
-        'l10n.cl.sii.activity.description',
+        'res.partner',
         string="Giro",required=True)
     pruebas_id= fields.One2many(
         'new_module.pruebas','proveedor_id',string='Borradores'
