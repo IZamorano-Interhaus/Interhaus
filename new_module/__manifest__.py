781dcaec7a20c18e8e9d575e25821797c6b48b9a
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-03-29 10:25:56 -0300

    se agrega dependencia a contacto, l10n_cl y account

--- new_module/__manifest__.py
+++ new_module/__manifest__.py
@@ -18,7 +18,11 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    
+    'depends':[
+        'contacts',
+        'l10n_cl',
+        'account'
+    ],
 
     # always loaded
     'data': [
