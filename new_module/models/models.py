f121b4fec3b17bdd10065494cafc07029ba7c09f
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-04-03 11:57:34 -0400

    actualizacion, testing cambio, l10n_cl_sii.....

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -175,7 +175,7 @@ class proveedores(models.Model):
         default='1', string = 'Tipo de contribuyente'
     )
     l10n_cl_sii_activity_description_id= fields.Many2one(
-        'l10n_cl.sii.activity.description',
+        'l10n_cl_sii_activity_description',
         string="Giro",required=True)
     pruebas_id= fields.One2many(
         'new_module.pruebas','proveedor_id',string='Borradores'
