f66f3628f8d0b0fbb04dae92bec07bdb82d08a8e
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-04-03 08:31:07 -0400

    actualizacion, cambio en el campo direccion, forma de obtener un dato en especifico

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -156,7 +156,8 @@ class proveedores(models.Model):
     _name="new_module.proveedores"
     _description="borrador para los proveedores"
     name=fields.Many2one('res.partner',string = "Nombre proveedor", required=True)
-    address=fields.Many2one('res.partner',string="Dirección",required=True)
+
+    address=fields.Many2one('res.partner','address',string="Dirección",required=True)
     company_id=fields.Many2one(
         string="Compañia",
         comodel_name="res.company",
