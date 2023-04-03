1251272d65becd4bf994a6e87c9e5c3b887200c0
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-04-03 12:38:44 -0400

    actualizacion, testing cambio, res_partner

--- new_module/models/models.py
+++ new_module/models/models.py
@@ -175,7 +175,7 @@ class proveedores(models.Model):
         default='1', string = 'Tipo de contribuyente'
     )
     l10n_cl_sii_activity_description_id= fields.Many2one(
-        'res_partner',
+        'res.partner',
         string="Giro",required=True)
     pruebas_id= fields.One2many(
         'new_module.pruebas','proveedor_id',string='Borradores'
