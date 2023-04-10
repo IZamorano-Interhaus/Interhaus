8f58c4341a5cd8bd294257ecc1b7f12760442216
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-04-10 13:44:25 -0400

    se recupera archivo perdido

--- new_module/__manifest__.py
+++ new_module/__manifest__.py
@@ -29,7 +29,8 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         
-        'views/templates.xml'
+        'views/templates.xml',
+        
         
     ],
     # only loaded in demonstration mode
