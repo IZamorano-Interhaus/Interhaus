2f302cbb366ad5aa1e2f9585048d0f9556d3bd81
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-04-10 17:55:57 -0400

    se agrega la herencia de res user

--- new_module/models/res_users.py
+++ new_module/models/res_users.py
@@ -2,6 +2,7 @@ from odoo import fields, models
 
 class res_users(models.Model):
     _name = "res.users"
+    _inherit="res.users"
     _description="usuario"
 
     new_field = fields.Char(string="New Field")
