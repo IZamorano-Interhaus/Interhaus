f3847bcd60026471fcaf5501c69ae36194ccd90e
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-04-12 16:11:00 -0400

    actualizacion, se referencia la vista para su onvocacion

--- new_module/__manifest__.py
+++ new_module/__manifest__.py
@@ -28,7 +28,8 @@
     
         'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml'
+        'views/templates.xml',
+        'views/libro_compras.xml'
         
         
     ],
