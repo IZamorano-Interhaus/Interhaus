dcc296a9f2ced7fabeda01efe3b805aacfbe2125
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-04-14 16:28:00 -0400

    actualizacion, se agrega herencia en libro compras

--- new_module/models/libro_compras.py
+++ new_module/models/libro_compras.py
@@ -2,6 +2,7 @@ from odoo import fields,models,api,_
 from datetime import  date
 
 class informe(models.Model):
+    _inherit = "account.move"
     _name="new_module.informe"
     _description="modulo para descargar archivos en pdf y vista preliminar de libros contables"
     @api.depends('parent_id', 'parent_id.level')
