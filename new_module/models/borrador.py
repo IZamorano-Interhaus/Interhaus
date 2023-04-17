34f71fdaaf2a7af8df196ce2e9515b91ab12a2b9
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-04-17 08:34:46 -0400

    actualizacion, se cambia el nombre del libro copy a borrador, se trabajara con el mas tarde, cambios en el codigo de la vista los nombres

---
+++ new_module/models/borrador.py
@@ -0,0 +1,10 @@
+from odoo import fields,models,api,_
+from datetime import  date
+
+class borradores(models.Model):
+    
+    _name="new_module.borrador"
+    _description="modulo para crear tabla borrador para alojar los datos que se obtienen en facturas"
+
+    
+    
--- new_module/models/libro_compras copy.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields,models,api,_
-from datetime import  date
-
-class informe(models.Model):
-    _inherit = "account.move"
-    _name="new_module.informe"
-    _description="modulo para descargar archivos en pdf y vista preliminar de libros contables"
-    
