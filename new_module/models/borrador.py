a5077de149d9baf3adec31d4cc70bf282b32d5a9
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-04-17 11:41:45 -0400

    actualizacion, borrador, se agrega propiedades a la seleccion

--- new_module/models/borrador.py
+++ new_module/models/borrador.py
@@ -6,31 +6,55 @@ class borradoresFactura(models.Model):
     _name="new_module.borrador.factura"
     _description="modulo para crear tabla borrador para alojar los datos que se obtienen en facturas"
 
-    tipoDTEString= fields.Char(),
-    tipoDTE= fields.Integer(),
-    tipoCompra= fields.Char(),
-    rutCliente= fields.Char(),
-    razonSocial= fields.Char(),
-    folio= fields.Integer(),
-    fechaEmision= fields.Date(),
-    fechaRecepcion = fields.Date(),
-    acuseRecibo= fields.Selection(),
-    montoExento = fields.Float(),
-    montoNeto= fields.Float(),
-    montoIvaRecuperable =  fields.Float(),
-    montoTotal= fields.Float() ,
-    ivaNoRetenido= fields.Float(),
-    totalOtrosImpuestos=  fields.Float(),
-    valorOtroImpuesto= fields.Char(),
-    tasaOtroImpuesto = fields.Char(),
-    tipoDocReferencia = fields.Integer(),
-    trackId= fields.Integer(),
-    referencias= fields.Char(),
-    referenciado=fields.Char() ,
-    reparos=fields.Char() ,
-    otrosImpuestos=fields.Char() ,
-    estado=fields.Selection() 
-    
+    tipoDTEString= fields.Char()
+    tipoDTE= fields.Integer()
+    tipoCompra= fields.Char()
+    rutCliente= fields.Char()
+    razonSocial= fields.Char()
+    folio= fields.Integer()
+    fechaEmision= fields.Date()
+    fechaRecepcion = fields.Date()
+    acuseRecibo= fields.Selection(selection=[
+            ('1','Pendiente de ser enviado'),
+            ('2','Aceptado'),
+            ('3','Consultar Estado Doc'),
+            ('4','Aceptado con reparos'),
+            ('5','Anulado'),
+            ('6','Rechazado'),
+            ('7','Manual'),
+        ],
+        string='acusoRecibo',
+        default='7',
+        required=True,
+        copy=False,)
+    montoExento = fields.Float()
+    montoNeto= fields.Float()
+    montoIvaRecuperable =  fields.Float()
+    montoTotal= fields.Float() 
+    ivaNoRetenido= fields.Float()
+    totalOtrosImpuestos=  fields.Float()
+    valorOtroImpuesto= fields.Char()
+    tasaOtroImpuesto = fields.Char()
+    tipoDocReferencia = fields.Integer()
+    trackId= fields.Integer()
+    referencias= fields.Char()
+    referenciado=fields.Char() 
+    reparos=fields.Char() 
+    otrosImpuestos=fields.Char() 
+    estado=fields.Selection(selection=[
+            ('1','Pendiente de ser enviado'),
+            ('2','Aceptado'),
+            ('3','Consultar Estado Doc'),
+            ('4','Aceptado con reparos'),
+            ('5','Anulado'),
+            ('6','Rechazado'),
+            ('7','Manual'),
+        ],
+        string='acusoRecibo',
+        default='7',
+        required=True,
+        copy=False,) 
+    #funciones
 
 
 
@@ -38,13 +62,25 @@ class borradorDetalle(models.Model):
     _name="new_module.borrador.detalle"
     _description="Muestra la cantidad de documentos emitidos segun el tipo de documento"
 
-    tipoDte= fields.Integer(),
-    tipoDteString= fields.Char(),
-    totalDocumentos=fields.Integer(),
-    montoExento=fields.Integer(),
-    montoNeto=fields.Integer(),
-    ivaRecuperable=fields.Float(), 
-    ivaUsoComun= fields.Float(),
-    ivaNoRecuperable=fields.Float(),
-    montoTotal=fields.Float(),
-    estado=fields.Selection()
+    tipoDte= fields.Integer()
+    tipoDteString= fields.Char()
+    totalDocumentos=fields.Integer()
+    montoExento=fields.Integer()
+    montoNeto=fields.Integer()
+    ivaRecuperable=fields.Float() 
+    ivaUsoComun= fields.Float()
+    ivaNoRecuperable=fields.Float()
+    montoTotal=fields.Float()
+    estado=fields.Selection(selection=[
+            ('1','Pendiente de ser enviado'),
+            ('2','Aceptado'),
+            ('3','Consultar Estado Doc'),
+            ('4','Aceptado con reparos'),
+            ('5','Anulado'),
+            ('6','Rechazado'),
+            ('7','Manual'),
+        ],
+        string='acusoRecibo',
+        default='7',
+        required=True,
+        copy=False,)
