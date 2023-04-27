20dff8183f427aec43283df05db4670549f22741
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-04-27 14:48:55 -0400

    P.Account l24, l25

--- Purchase_into_account/models/purchase_account.py
+++ Purchase_into_account/models/purchase_account.py
@@ -9,18 +9,35 @@ class reporte_detalle(models.Model):
 class OrdenCompra(models.Model):
     _inherit=['purchase.order']
     
-    x_studio_cuenta_contable=fields.Many2one('account.account',compute="_get_cuenta_contable")
-    
-    name=fields.Text(string='N°OC', required=True, compute='_get_name', store=True, readonly=True)
+    x_studio_cuenta_contable=fields.Many2one('account.account',
+                                             compute="_get_cuenta_contable"
+                                             )
+    name=fields.Text(
+        string='N°OC',
+        required=True,
+        compute='_get_name',
+        store=True,
+        readonly=True
+        )
 
-    x_studio_many2one_field_w1OXM=fields.Many2one('account.analytic.account', compute="_get_centro_negocio")
+    x_studio_many2one_field_w1OXM=fields.Many2one(
+        'account.analytic.account'#, 
+        #compute="_get_centro_negocio"
+        )
     
-    partner_id=fields.Many2one('res.partner',compute="partner_id")
-    amount_total=fields.Monetary(compute="_get_amount_total")
+    partner_id=fields.Many2one(
+        'res.partner',
+        compute="partner_id"
+        )
+    amount_total=fields.Monetary(
+        compute="_get_amount_total"
+        )
     invoice_status=fields.Selection(selection=[
             ('no','Nada para facturar'),
             ('to invoice','Para Facturar'),
-            ('invoiced','Totalmente facturado')],compute="_get_invoice_status")
+            ('invoiced','Totalmente facturado')],
+            compute="_get_invoice_status"
+            )
 
     """ @api.depends('currency_id','company_id','partner_id')
     def _get_name(self):
