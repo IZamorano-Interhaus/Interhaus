172593a225b35840a4fa9ed25be525b0d2b5e6de
izamorano-interhaus <na.zamovasquez@gmail.com>
2023-04-27 12:52:32 -0400

    l12

--- Purchase_into_account/models/purchase_account.py
+++ Purchase_into_account/models/purchase_account.py
@@ -9,7 +9,7 @@ class reporte_detalle(models.Model):
 class OrdenCompra(models.Model):
     _inherit=['purchase.order']
 
-    name=fields.Char(string = "N°OC" ,compute="_get_name")
+    #name=fields.Char(compute="_get_name")
     x_studio_many2one_field_w1OXM=fields.Many2one('account.analytic.account', compute="_get_centro_negocio")
     x_studio_cuenta_contable=fields.Many2one('account.account',compute="_get_cuenta_contable")
     
@@ -30,8 +30,8 @@ class OrdenCompra(models.Model):
         self.env.cr.execute(query)
         res=self.env.cr.fetchone()
         
-        for product in self:
-            id = product.id
+        for OC in self:
+            id = OC.id
             result = {id: {'name': res}}
         return result
     
